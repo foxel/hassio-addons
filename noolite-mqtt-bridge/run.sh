bd36944122a351a260ebc1e3da8e98e7bdeee14e
Andrey F. Kupreychik <foxel@quickfox.ru>
2018-11-29 07:10:36 +0700

    v0.0.4

--- noolite-mqtt-bridge/run.sh
+++ noolite-mqtt-bridge/run.sh
@@ -7,5 +7,7 @@ serial_device=$(jq --raw-output ".serial_device" $CONFIG_PATH)
 mqtt_host=$(jq --raw-output ".mqtt_host" $CONFIG_PATH)
 mqtt_port=$(jq --raw-output ".mqtt_port" $CONFIG_PATH)
 mqtt_prefix=$(jq --raw-output ".mqtt_prefix" $CONFIG_PATH)
+username=$(jq --raw-output ".username" $CONFIG_PATH)
+password=$(jq --raw-output ".password" $CONFIG_PATH)
 
-exec /opt/noolite/main.py "${serial_device}" "${mqtt_prefix}" "${mqtt_host}" "${mqtt_port}"
+exec noolite-mqtt "${serial_device}" "${mqtt_prefix}" "${mqtt_host}" "${username}" "${password}" -p "${mqtt_port}"
